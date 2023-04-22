9f0147a444f3ab3f393686dcb77ddbdc3fe5822b
William Hsieh <limelego@gmail.com>
2023-04-21 23:47:38 -0700

    ios15 instead of 16

--- demo_1/lib/main.dart
+++ demo_1/lib/main.dart
@@ -1,43 +1,79 @@
-// import 'package:location/location.dart';
-import 'package:sensors_plus/sensors_plus.dart';
+import 'package:flutter/material.dart';
 
-Future<String> createOrderMessage() async {
-  var order = await fetchUserOrder();
-  return 'Your order is: $order';
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({required this.title, super.key});
+
+  // Fields in a Widget subclass are always marked "final".
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0, // in logical pixels
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      // Row is a horizontal, linear layout.
+      child: Row(
+        children: [
+          const IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null, // null disables the button
+          ),
+          // Expanded expands its child
+          // to fill the available space.
+          Expanded(
+            child: title,
+          ),
+          const IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-Future<String> fetchUserOrder() =>
-    // Imagine that this function is
-    // more complex and slow.
-    Future.delayed(
-      const Duration(seconds: 2),
-      () => 'Large Latte',
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Material is a conceptual piece
+    // of paper on which the UI appears.
+    return Material(
+      // Column is a vertical, linear layout.
+      child: Column(
+        children: [
+          MyAppBar(
+            title: Text(
+              'Voyager',
+              style: Theme.of(context) //
+                  .primaryTextTheme
+                  .titleLarge,
+            ),
+          ),
+          const Expanded(
+            child: Center(
+              child: Text('Welcome to Voyager'),
+            ),
+          ),
+        ],
+      ),
     );
+  }
+}
 
-
-Future<void> main() async {
-  // //print('Fetching user order...');\
-  // var location = Location();
-  // final locationdata = await location.getLocation();
-  // print("Location: ${locationdata.latitude}, ${locationdata.longitude}");
-  // print(await createOrderMessage());
-
-  userAccelerometerEvents.listen((UserAccelerometerEvent event) {
-  print(event);
-});
-// [UserAccelerometerEvent (x: 0.0, y: 0.0, z: 0.0)]
-
-accelerometerEvents.listen((AccelerometerEvent event) {
-  print(event);
-});
-// [AccelerometerEvent (x: 0.0, y: 9.8, z: 0.0)]
-
-gyroscopeEvents.listen((GyroscopeEvent event) {
-  print(event);
-});
-// [GyroscopeEvent (x: 0.0, y: 0.0, z: 0.0)]
-
-magnetometerEvents.listen((MagnetometerEvent event) {
-  print(event);
-});
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'My app', // used by the OS task switcher
+      home: SafeArea(
+        child: MyScaffold(),
+      ),
+    ),
+  );
 }
