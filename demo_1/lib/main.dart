559ef639f1b9006ac277cc7b8fc3e06a7673b8b7
Derek Hua <derekhua@ucla.edu>
2023-04-22 00:57:28 -0700

    Added firebase to main

--- demo_1/lib/main.dart
+++ demo_1/lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 // import 'welcome.dart';
 import 'home.dart';
 
+
+void test() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
+
 void main() {
+  test();
   return runApp(
       const MaterialApp(
       title: 'My app', // used by the OS task switcher
